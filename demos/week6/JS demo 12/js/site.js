9766e3755637db09917e81643f26a8231ebc8ea4
dkriegls <dkriegls@gmail.com>
2020-10-15 18:42:11 -0500

    demo updates

--- demos/week6/JS demo 12/js/site.js
+++ demos/week6/JS demo 12/js/site.js
@@ -2,14 +2,6 @@
 jQuery(function ($) {
     $('html').removeClass('nojs');
     $("html").addClass("hasjs");
-    $("#email").on("focus", function () {
-        $("#input-email label").addClass("active");
-    });
-    $("#email").on("blur", function () {
-        if ($("#email").val().length === 0) {
-            $("#input-email label").removeClass("active");
-        }
-    });
     $("#alt-box").on("click", function () {
         $("#age-verification").prop("checked", true);
         $("#alt-box").html("<p>&#10004;</p>");
